608d2445d7fcb357e4d71b2e69c8aff5d27485da
cott21 <cott21@cott21-desktop.(none)>
2012-05-15 15:47:34 -0400

    Update receiver_monitor

--- build/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/gr-gtlib/include
-# Build directory: /opt/gr-gtlib/build/include
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/python/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /opt/gr-gtlib/python
-# Build directory: /opt/gr-gtlib/build/python
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
