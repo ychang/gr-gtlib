ddde79dec58e7c86c0268d716c16c0be0d206b8d
COTT21 <cott21@COTT21.(none)>
2012-07-03 15:11:01 -0400

    Update OFDM-STBC Modules

--- apps/OFDM/benchmark_tx.py
+++ apps/OFDM/benchmark_tx.py
@@ -111,7 +111,7 @@ def main():
         if options.discontinuous and pktno % 5 == 4:
             time.sleep(1)
             
-        time.sleep(0.5)            
+        time.sleep(0.05)            
         pktno += 1
         
     send_pkt(eof=True)
